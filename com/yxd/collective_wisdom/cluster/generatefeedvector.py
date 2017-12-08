70538bc493311c45e991902dbe7623264b5e10d7
yxdshine <659777399@qq.com>
2017-12-08 17:15:25 +0800

    集体智慧 feedparser 安装

---
+++ com/yxd/collective_wisdom/cluster/generatefeedvector.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+"""
+python pip 安装教程：https://jingyan.baidu.com/article/e73e26c0d94e0524adb6a7ff.html
+        因为是用的anaconda3的Python3处理的，切换到anaconda3的目录下D:\yxd\tool\anaconda\anaconda3\path\Scripts
+        easy_install.exe pip
+        pip install feedparser
+
+feedparser官网：安装教程
+https://pypi.python.org/pypi/feedparser
+解决问题：https://www.cnblogs.com/space-place/p/6260521.html
+"""
+
+import feedparser
+import re
+
